ba4d7cb8ed8f69d1ecd1829e551c753855027820
蒟蒻本弱 <93389541+yirenlove@users.noreply.github.com>
2024-09-17 16:21:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,15 +25,15 @@ export default defineConfig({
     }
   },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace('/api', '')
-      },
-      '/admin': {
-        target: 'http://127.0.0.1:8080',
-        changeOrigin: true
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://127.0.0.1:5000',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace('/api', '')
+    //   },
+    //   '/admin': {
+    //     target: 'http://127.0.0.1:8080',
+    //     changeOrigin: true
       }
     }
   }
